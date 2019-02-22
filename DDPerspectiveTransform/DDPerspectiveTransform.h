bf35379bf39a77ec892b099df9e64aaa2adb4c3c
Доценко Дмитрий <dotsenko.d.r@sberbank.ru>
2019-02-22 12:09:52 +0300

    Initial Commit

---
+++ DDPerspectiveTransform/DDPerspectiveTransform.h
@@ -0,0 +1,19 @@
+//
+//  DDPerspectiveTransform.h
+//  DDPerspectiveTransform
+//
+//  Created by Доценко Дмитрий on 22/02/2019.
+//  Copyright © 2019 Dotsenko-D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DDPerspectiveTransform.
+FOUNDATION_EXPORT double DDPerspectiveTransformVersionNumber;
+
+//! Project version string for DDPerspectiveTransform.
+FOUNDATION_EXPORT const unsigned char DDPerspectiveTransformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDPerspectiveTransform/PublicHeader.h>
+
+
